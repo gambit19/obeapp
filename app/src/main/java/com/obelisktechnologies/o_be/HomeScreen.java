c47bb77e76e9bdfb464ec687c591133f6ac7092f
Rahul <rahulk1991@gmail.com>
2017-05-31 15:48:26 +0530

    First commit

---
+++ app/src/main/java/com/obelisktechnologies/o_be/HomeScreen.java
@@ -0,0 +1,28 @@
+package com.obelisktechnologies.o_be;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_home_screen);
+    }
+
+    public void homeFunction(View view){
+
+        Intent intent = new Intent(getApplicationContext(),HomeOptions.class);
+        startActivity(intent);
+
+     }
+}
